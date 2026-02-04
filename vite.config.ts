de4843897c0d073a7b626725d35e0d774c8ff8f8
mkmodus <38850259+mkmodus@users.noreply.github.com>
2026-02-05 02:21:58 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // 경고 기준을 500kB에서 1000kB로 높여줍니다.
+    rollupOptions: {
+      output: {
+        // 대형 라이브러리들을 별도의 파일로 분리하여 효율을 높입니다.
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
